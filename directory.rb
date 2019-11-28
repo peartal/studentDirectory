3a14dc9a1c67d8c60abf195d6c602bb9324b3ff8
adrian harvey <adrian.harvey@vodafone.com>
2019-11-28 15:25:05 +0000

    each statement added

--- directory.rb
+++ directory.rb
@@ -1,15 +1,28 @@
+
+
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+ ]
+
+
+
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# below we show the amount of students
-print "Overall, we have\n11\ngreat students\n"
+
+
+students.each do |student|
+    puts student
+end
+
+puts "Overall, we have #{students.count} great students"
+
