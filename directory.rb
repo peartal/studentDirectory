a0e30911e2dc006cf0fdc56c74a877c3e3791751
adrian harvey <adrian.harvey@vodafone.com>
2019-11-28 16:12:44 +0000

    added index method

--- directory.rb
+++ directory.rb
@@ -35,8 +35,10 @@ def print_header
 end
 
 def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)" 
+    students.each_with_index do |student, index|
+        if student[:name][0] == 'M'
+         puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)" 
+        end
     end
 end
 
